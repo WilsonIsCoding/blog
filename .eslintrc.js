f50b2930dded9b3c572c577d18862308854fccd7
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2024-02-19 15:58:26 +0800

    [✨ feat]：eslint done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['eason', 'next/core-web-vitals', 'plugin:prettier/recommended'],
+  extends: [
+    'eason',
+    'next/core-web-vitals',
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['tailwindcss'],
   rules: {
     'jsx-a11y/anchor-is-valid': [
       'error',
@@ -9,20 +15,26 @@ module.exports = {
         aspects: ['invalidHref', 'preferButton'],
       },
     ],
+    'tailwindcss/classnames-order': 'off', // Respect prettier-plugin-tailwindcss order
   },
-  overrides: [
-    {
-      files: '**/*.{ts,tsx}',
-      extends: ['eason/typescript', 'plugin:prettier/recommended'],
-    },
-  ],
   settings: {
-    'import/ignore': ['contentLayerAdapter.js'],
+    // Support absolute imports
+    // https://www.npmjs.com/package/eslint-import-resolver-alias
     'import/resolver': {
       alias: {
         map: [['@', './src']],
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
+    'import/ignore': ['contentLayerAdapter.js'],
   },
+  overrides: [
+    {
+      files: '**/*.{ts,tsx}',
+      extends: [
+        'eason/typescript',
+        'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+      ],
+    },
+  ],
 };
