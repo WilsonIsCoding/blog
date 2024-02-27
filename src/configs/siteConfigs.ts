e03841fec7f6961118a5c7a24a20de22fbf3f6ac
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2024-02-28 01:06:57 +0800

    feature：SEO config

---
+++ src/configs/siteConfigs.ts
@@ -0,0 +1,17 @@
+const fqdn = 'https://nextjs-blog-ashen-nine-78.vercel.app/';
+const logoPath = '/logo.png';
+const bannerPath = '/og-image.png';
+
+export const siteConfigs = {
+  title: 'Next.js Tailwind ContentLayer Blog Starter',
+  titleShort: 'Next Blog',
+  description:
+    'Blog starter template with modern frontend technologies like Next.js, Tailwind CSS, ContentLayer, i18Next',
+  author: 'Wilson Cheng',
+  fqdn: fqdn,
+  logoPath: logoPath,
+  logoUrl: fqdn + logoPath,
+  bannerPath: bannerPath,
+  bannerUrl: fqdn + bannerPath,
+  datePublished: '2022-09-01',
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,50 @@ import '@/styles/prism-plus.css';
 
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { DefaultSeo } from 'next-seo';
 import { ThemeProvider } from 'next-themes';
+
+import { siteConfigs } from '@/configs/siteConfigs';
 
 import LayoutWrapper from '../components/LayoutWrapper';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
-      </Head>
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
+      <DefaultSeo
+        titleTemplate={`%s|${siteConfigs.titleShort}`}
+        defaultTitle={siteConfigs.title}
+        canonical={siteConfigs.fqdn}
+        openGraph={{
+          title: siteConfigs.title,
+          description: siteConfigs.description,
+          url: siteConfigs.fqdn,
+          images: [
+            {
+              url: siteConfigs.bannerUrl,
+            },
+          ],
+          site_name: siteConfigs.title,
+          type: 'website',
+        }}
+        additionalMetaTags={[
+          {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1',
+          },
+        ]}
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            href: siteConfigs.logoPath,
+          },
+        ]}
+      >
+        {' '}
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </DefaultSeo>
     </ThemeProvider>
   );
 }
