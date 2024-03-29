584c741babf55bc3700acbc0fa910711500b81fa
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2024-03-29 15:34:40 +0800

    feature：self-introduce adjust

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,8 +38,11 @@ const Home: NextPage<Props> = ({ posts }) => {
 
       <div className="prose my-12 space-y-2 transition-colors dark:prose-dark md:prose-lg md:space-y-5">
         <h1 className="text-center sm:text-left">嗨 I'm Wilson</h1>
-        <p>前端初學第二年，喜歡各種新技術</p>
-        <p>喜歡煮飯、跑步和看書！和跟那顆排球一樣！</p>
+        <p>前端初學第二年，喜歡新奇的東西所以喜歡前端</p>
+        <p>
+          喜歡煮飯到每個禮拜會去市場採買兩次、喜歡跑步到每年會固定參加半馬馬拉松、喜歡看書到每個禮拜固定看完兩本書！
+          和跟那顆排球一樣！歡迎你到這裡，看看最近的我在學些什麼
+          </p>
       </div>
 
       <div className="my-4 divide-y divide-gray-200 transition-colors dark:divide-gray-700">
